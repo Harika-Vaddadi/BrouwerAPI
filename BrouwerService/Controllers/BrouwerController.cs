0504781da2d63885a6544f4e64564a758ff5ee99
Harika Vaddadi <harika.vaddadi@vdabcampus.be>
2021-03-19 15:48:48 +0100

    Made methods async

--- BrouwerService/Controllers/BrouwerController.cs
+++ BrouwerService/Controllers/BrouwerController.cs
@@ -18,76 +18,138 @@ namespace BrouwerService.Controllers
         public BrouwerController(IBrouwerRepository repository) => this.repository = repository;
 
         [HttpGet] 
-        public ActionResult FindAll() => base.Ok(repository.FindAll());
-
-        [HttpGet("{id}")] // In url you type or in Postman type: localhost:5000/brouwers/<id number>
-        public ActionResult FindById(int id) 
-        {
-            var brouwer = repository.FindById(id); 
+        public async Task<ActionResult> FindAll() => base.Ok(await repository.FindAllAsync());
+        [HttpGet("{id}")] public async Task<ActionResult> FindById(int id) 
+        { 
+            var brouwer = await repository.FindByIdAsync(id); 
             return brouwer == null ? base.NotFound() : base.Ok(brouwer); 
         }
-
-        [HttpGet("naam")] // //localhost:5000/brouwers/naam?begin=<give the begin string>
-        public ActionResult FindByBeginNaam(string begin) => base.Ok(repository.FindByBeginNaam(begin));
-
-        //[HttpGet("{jaar}/{maand}")]
-        //public ActionResult FindStatistieken(int jaar, int maand)
-        //{  
-        //}
+        [HttpGet("naam")] 
+        public async Task<ActionResult> FindByBeginNaam(string begin) => 
+                                                                   base.Ok(await repository.FindByBeginNaamAsync(begin));
 
         [HttpDelete("{id}")] 
-        public ActionResult Delete(int id) 
+        public async Task<ActionResult> Delete(int id) 
         { 
-            var brouwer = repository.FindById(id);
-            if (brouwer == null)   
-            {
+            var brouwer = await repository.FindByIdAsync(id); 
+            if (brouwer == null) 
+            { 
                 return base.NotFound(); 
-            }
-            repository.Delete(brouwer); 
+            } 
+            await repository.DeleteAsync(brouwer); 
             return base.Ok(); 
         }
-
         [HttpPost] 
-        public ActionResult Post(Brouwer brouwer)  
-        {
+        public async Task<ActionResult> Post(Brouwer brouwer) 
+        { 
             if (this.ModelState.IsValid)
-            {
-                repository.Insert(brouwer);
-                return base.CreatedAtAction(nameof(FindById), new
-                {
-                    id = brouwer.Id
+            { 
+                await repository.InsertAsync(brouwer); 
+                return base.CreatedAtAction(nameof(FindById), new 
+                { 
+                    id = brouwer.Id 
                 },
-                null);
-            }
-            //return base.BadRequest();
-            return base.BadRequest(this.ModelState);
+                null); 
+            } 
+            return base.BadRequest(this.ModelState); 
         }
-
         [HttpPut("{id}")] 
-        public ActionResult Put(int id, Brouwer brouwer) 
-        {
-            if (this.ModelState.IsValid)
-            {
-                try  
+        public async Task<ActionResult> Put(int id, Brouwer brouwer) 
+        { 
+            if (this.ModelState.IsValid) 
+            { 
+                try 
                 { 
                     brouwer.Id = id; 
-                    repository.Update(brouwer); 
+                    await repository.UpdateAsync(brouwer); 
                     return base.Ok(); 
-                }
-                catch (DbUpdateConcurrencyException)   
-                {
+                } 
+                catch (DbUpdateConcurrencyException) 
+                { 
                     return base.NotFound(); 
                 } 
                 catch 
-                {
+                { 
                     return base.Problem(); 
-                }
-            }
-            //return base.BadRequest();
-            return base.BadRequest(this.ModelState);
-        } 
+                } 
+            } 
+            return base.BadRequest(this.ModelState); 
+        }
+
 
 
 
     }
 }
+
+
+
+//[HttpGet]
+//public ActionResult FindAll() => base.Ok(repository.FindAll());
+
+//[HttpGet("{id}")] // In url you type or in Postman type: localhost:5000/brouwers/<id number>
+//public ActionResult FindById(int id)
+//{
+//    var brouwer = repository.FindById(id);
+//    return brouwer == null ? base.NotFound() : base.Ok(brouwer);
+//}
+
+//[HttpGet("naam")] // //localhost:5000/brouwers/naam?begin=<give the begin string>
+//public ActionResult FindByBeginNaam(string begin) => base.Ok(repository.FindByBeginNaam(begin));
+
+////[HttpGet("{jaar}/{maand}")]
+////public ActionResult FindStatistieken(int jaar, int maand)
+////{  
+////}
+
+//[HttpDelete("{id}")]
+//public ActionResult Delete(int id)
+//{
+//    var brouwer = repository.FindById(id);
+//    if (brouwer == null)
+//    {
+//        return base.NotFound();
+//    }
+//    repository.Delete(brouwer);
+//    return base.Ok();
+//}
+
+//[HttpPost]
+//public ActionResult Post(Brouwer brouwer)
+//{
+//    if (this.ModelState.IsValid)
+//    {
+//        repository.Insert(brouwer);
+//        return base.CreatedAtAction(nameof(FindById), new
+//        {
+//            id = brouwer.Id
+//        },
+//        null);
+//    }
+//    //return base.BadRequest();
+//    return base.BadRequest(this.ModelState);
+//}
+
+//[HttpPut("{id}")]
+//public ActionResult Put(int id, Brouwer brouwer)
+//{
+//    if (this.ModelState.IsValid)
+//    {
+//        try
+//        {
+//            brouwer.Id = id;
+//            repository.Update(brouwer);
+//            return base.Ok();
+//        }
+//        catch (DbUpdateConcurrencyException)
+//        {
+//            return base.NotFound();
+//        }
+//        catch
+//        {
+//            return base.Problem();
+//        }
+//    }
+//    //return base.BadRequest();
+//    return base.BadRequest(this.ModelState);
+//}
--- BrouwerService/Repositories/BrouwerRepository.cs
+++ BrouwerService/Repositories/BrouwerRepository.cs
@@ -10,27 +10,50 @@ namespace BrouwerService.Repositories
     public class BrouwerRepository : IBrouwerRepository
     {
         private readonly BierlandContext context; 
-        public BrouwerRepository(BierlandContext context) => this.context = context; 
-        public IQueryable<Brouwer> FindAll() => context.Brouwers.AsNoTracking(); 
-        public Brouwer FindById(int id) => context.Brouwers.Find(id); 
-        public IQueryable<Brouwer> FindByBeginNaam(string begin) => context.Brouwers.AsNoTracking()
-                                                                   .Where(brouwer => brouwer.Naam.StartsWith(begin)); 
-        public void Insert(Brouwer brouwer) 
+        public BrouwerRepository(BierlandContext context) => this.context = context;
+        public async Task DeleteAsync(Brouwer brouwer) 
         { 
-            context.Brouwers.Add(brouwer); 
-            context.SaveChanges(); 
+            context.Brouwers.Remove(brouwer); 
+            await context.SaveChangesAsync(); 
         }
-        public void Delete(Brouwer brouwer) 
+        public async Task<List<Brouwer>> FindAllAsync() => await context.Brouwers.AsNoTracking().ToListAsync(); 
+        public async Task<List<Brouwer>> FindByBeginNaamAsync(string begin) => await context.Brouwers.AsNoTracking()
+                                                                    .Where(brouwer => brouwer.Naam.StartsWith(begin))
+                                                                    .ToListAsync(); 
+        public async Task<Brouwer> FindByIdAsync(int id) => await context.Brouwers.FindAsync(id); 
+        public async Task InsertAsync(Brouwer brouwer) 
         { 
-            context.Brouwers.Remove(brouwer);
-            context.SaveChanges(); 
+            await context.Brouwers.AddAsync(brouwer); 
+            await context.SaveChangesAsync(); 
         }
-        public void Update(Brouwer brouwer) 
+        public async Task UpdateAsync(Brouwer brouwer) 
         { 
             context.Brouwers.Update(brouwer); 
-            context.SaveChanges(); 
+            await context.SaveChangesAsync(); 
         }
 
-        
+
     }
 }
+
+
+
+//public IQueryable<Brouwer> FindAll() => context.Brouwers.AsNoTracking();
+//public Brouwer FindById(int id) => context.Brouwers.Find(id);
+//public IQueryable<Brouwer> FindByBeginNaam(string begin) => context.Brouwers.AsNoTracking()
+//                                                           .Where(brouwer => brouwer.Naam.StartsWith(begin));
+//public void Insert(Brouwer brouwer)
+//{
+//    context.Brouwers.Add(brouwer);
+//    context.SaveChanges();
+//}
+//public void Delete(Brouwer brouwer)
+//{
+//    context.Brouwers.Remove(brouwer);
+//    context.SaveChanges();
+//}
+//public void Update(Brouwer brouwer)
+//{
+//    context.Brouwers.Update(brouwer);
+//    context.SaveChanges();
+//}
